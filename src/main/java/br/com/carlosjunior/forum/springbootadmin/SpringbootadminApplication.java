cc4f9cdc56d9785df201f1a13c2622034bb820b6
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-07-04 15:13:19 -0300

    SpringBoot-Admin created

---
+++ src/main/java/br/com/carlosjunior/forum/springbootadmin/SpringbootadminApplication.java
@@ -0,0 +1,18 @@
+package br.com.carlosjunior.forum.springbootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class SpringbootadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootadminApplication.class, args);
+	}
+
+}
